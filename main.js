84b47ac51072d295fe8829af925b5701645a9511
zhangchiqing <zhangchiqing@gmail.com>
2015-08-07 20:36:30 -0700

    scaffolding

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Rx = require('rx-lite');
+var h = require('virtual-dom/h');
+var diff = require('virtual-dom/diff');
+var patch = require('virtual-dom/patch');
+var createElement = require('virtual-dom/create-element');
+var _ = require('underscore');
+
